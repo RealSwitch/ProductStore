8a14c5bbbfcf37857b7ee97f015680ce8c8f3448
switch <semakenjhb024@student.wethinkcode.co.za>
2025-08-21 21:22:34 +0200

    removed hard coded port number to be configured in .env

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -5,11 +5,12 @@ import productRoutes from './routes/product.routes.js';
 
 dotenv.config();
 const app = express();
+const PORT = process.env.PORT || 5000;
 app.use(express.json());
 
 app.use('/api/products', productRoutes);
 console.log(process.env.MONGO_URI);
-app.listen(5000, () => {
+app.listen(PORT, () => {
     connectDB();
-    console.log('Server is running at http://localhost:5000');
+    console.log('Server is running at http://localhost: ' + PORT);
 });
