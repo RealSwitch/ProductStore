e30e4322e43de31fd072865eace9c62599852ab7
switch <semakenjhb024@student.wethinkcode.co.za>
2025-08-21 22:54:02 +0200

    Trying to render my frontend. Seems like i am facing issues with my ChakraProvider button which doesn't render. Need to check for  a version that will work.

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,12 @@
+
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      {/* <h1>Welcome to the Product Store</h1> */}
+      <Button >Click Me</Button>
+    </>
+  );
+}   
+export default App;
